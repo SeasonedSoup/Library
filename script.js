d1e31601c08e332ece5629a6dac6bb619e05fa49
SeasonedSoup <emmetteagoncillo1@gmail.com>
2024-11-22 08:24:32 +0800

    Create Book Button and Create Event on it
    once bookbuttosn been clicked we add a form
    possibly designed inside a modal that will ask
    the four required information to create the book
    I utilized an array and will probably use a for loop
    on it to dynamically create the three input questions
    except for the number of pages cause i might use boolean and
    use a dropdown there separately the ncreate a button that will
    submit this modal and probably call the function that will store
    this in the array

--- script.js
+++ script.js
@@ -6,11 +6,6 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     
-    this.info = function () {
-        let readStatus = this.read ? "already read" : "not read yet"
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`
-    }
-
 };
 
 function addBookToLibrary(title, author, pages, read) {
@@ -27,15 +22,28 @@ function createTableRows (myLibrary) {
 
         for(let key in book) {
             const tableData = document.createElement('td');
-            tableData.textContent = book[key]
-            tableRows.appendChild(tableData)
+            tableData.textContent = book[key];
+            tableRows.appendChild(tableData);
         }
 
-        tableBody.appendChild(tableRows)
+        tableBody.appendChild(tableRows);
    })
-   
+};
 
-}
+const newBookButton = document.createElement('button');
+newBookButton.textContent = 'Add A New Book!';
+const body = document.querySelector('body');
+body.appendChild(newBookButton);
 
+
+/*create a modal form*/
+newBookButton.addEventListener('click', () => {
+    const form = document.createElement('form')
+   const inputs = [
+    { label: 'Book Name', type: 'text', name: 'bookName', id: 'bookName'},
+    { label: 'Author', type: 'text', name: 'author', id: 'author' },
+    { label: 'Pages',  type: 'number', name: 'pages', id: 'pages'}
+   ];
+})
 
 
